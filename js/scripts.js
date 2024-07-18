371b2aa1b2e8f6fffe04642c7d4629c5ea78dce0
muhammadbayuan <mnegoro.bayu@gmail.com>
2024-07-19 00:31:29 +0700

    update js css html

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,7 @@
 // ini JavaScripts
+
+function updateResult() {
+    let weightValue = document.getElementById('input-berat-badan').value;
+    document.getElementById('bmi-result').innerHTML = weightValue;
+    console.log (parseInt(weightValue));
+}
